f7d852dbd716942778ea17b6bc70cfd365d27f5f
Egor Sobolev <egor.sobolev@xfel.eu>
2023-12-04 15:02:04 +0100

    Add files with packaging instruction and metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
-import setuptools
 import numpy as np
-
 from setuptools import Extension, setup
-from Cython.Build import cythonize
+
 
 extensions = [
     Extension(
@@ -13,12 +11,9 @@ extensions = [
         ],
         include_dirs=[np.get_include()],
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-        #extra_compile_args=["-fno-wrapv",  "-fno-strict-aliasing"],
     ),
 ]
 
 setup(
-    packages=setuptools.find_packages('src'),
-    package_dir={"": "src"},
-    ext_modules = cythonize(extensions),
+    ext_modules=extensions,
 )
